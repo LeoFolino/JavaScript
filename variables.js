5de0097aece2c0df990e5becbed8555856c8e131
LeoFolino <leonelfolinoing@gmail.com>
2024-08-29 09:21:46 -0300

    Clase Variables y Tipos de Datos

---
+++ variables.js
@@ -0,0 +1,13 @@
+let numero1
+
+numero1 = 10
+
+let numero2 = 20
+
+suma = numero1 + numero2
+
+console.log("Si!! Bienvenidos!!")
+console.log(numero1, numero2, suma)
+
+// Dato importante: darle un valor 'null' a una variable, explicitamente estoy diciendo que no va a tener ningun valor. Por otro lado,
+// si no le doy el valor a la variable, va a indicar "undefined" diciendo que no tiene ningun valor de momento hasta que le demos uno.
