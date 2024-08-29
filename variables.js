d698bd52b6c0d90bac60878efd722a1e8c33c66b
LeoFolino <leonelfolinoing@gmail.com>
2024-08-29 09:28:54 -0300

    Clase Concatenacion e Interpolacion

--- variables.js
+++ variables.js
@@ -11,3 +11,10 @@ console.log(numero1, numero2, suma)
 
 // Dato importante: darle un valor 'null' a una variable, explicitamente estoy diciendo que no va a tener ningun valor. Por otro lado,
 // si no le doy el valor a la variable, va a indicar "undefined" diciendo que no tiene ningun valor de momento hasta que le demos uno.
+
+let nombre = "Leito"
+let apellido = "Folino"
+
+console.log("Hola! Mi nombre es " + nombre + " " + apellido) // concatenado
+
+console.log(`Hola! Mi nombre es ${nombre} ${apellido}`)
