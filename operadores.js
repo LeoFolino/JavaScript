6e3941bba9fb81f53c3721b6792a180f758ecec2
LeoFolino <leonelfolinoing@gmail.com>
2024-08-30 01:41:50 -0300

    Operadores Relacionales

--- operadores.js
+++ operadores.js
@@ -1,3 +1,4 @@
+//---------------------OPERADORES ARITMETICOS---------------------------------------
 //variables
 let resultado
 let numero10 = 10
@@ -23,3 +24,31 @@ console.log("Modulo")
 resultado = 11 % 2
 console.log(`El resto fue de: ${resultado}`)
 
+//-----------------OPERADORES RELACIONALES-------------------------------------
+let resultadoRelacional = 10 < 5    // menor que
+let resultadoRelacional2 = 10 <= 5  // menor o igual que
+let resultadoRelacional3 = 10 > 5   // mayor que
+let resultadoRelacional4 = 10 >= 5  // mayor o igual que
+let resultadoRelacional5 = 10 == 10 // igual que
+let resultadoRelacional6 = "10" == 10 //    igual que (da V porque compara que el valor del dato es igual)
+let resultadoRelacional7 = "10" === 10 //   igual que (da F porque es una igualacion estricta donde ademas de comparar el dato, valida el tipo de dato)
+let resultadoRelacional8 = 10 != 10  //   distinto que
+let resultadoRelacional9 = 10 != 5  //   distinto que
+let resultadoRelacional10 = "10" != 10 //   distinto que (da F porque compara el tipo de dato)
+let resultadoRelacional11 = "10" !== 10 // distinto que (da V porque es una igualacion estricta donde ademas de comparar dato, valida el tipo de dato)
+let num1, num2
+num1 = 10, num2 = 5
+let resultadoRelacional12 = num1 > num2
+
+console.log(resultadoRelacional)
+console.log(resultadoRelacional2)
+console.log(resultadoRelacional3)
+console.log(resultadoRelacional4)
+console.log(resultadoRelacional5)
+console.log(resultadoRelacional6)
+console.log(resultadoRelacional7)
+console.log(resultadoRelacional8)
+console.log(resultadoRelacional9)
+console.log(resultadoRelacional10)
+console.log(resultadoRelacional11)
+console.log(resultadoRelacional12)
