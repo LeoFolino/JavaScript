1faf053b427e61377923f92eee73bc626711e299
LeoFolino <leonelfolinoing@gmail.com>
2024-08-30 01:25:46 -0300

    Operadores aritmeticos

---
+++ operadores.js
@@ -0,0 +1,25 @@
+//variables
+let resultado
+let numero10 = 10
+
+//resta
+console.log("Resta")
+resultado = 15 - numero10
+console.log(resultado)
+//multiplicacion
+console.log("Multiplicacion")
+resultado = numero10 * numero10
+console.log(resultado)
+//division
+console.log("Division")
+resultado = resultado / numero10
+console.log(resultado)
+//suma
+console.log("Suma")
+resultado = 15 + numero10
+console.log(resultado)
+//modulo
+console.log("Modulo")
+resultado = 11 % 2
+console.log(`El resto fue de: ${resultado}`)
+
