4924ae1548bbcc7dc13e0b1c3ebadac5424ae63e
LeoFolino <leonelfolinoing@gmail.com>
2024-09-02 09:04:07 -0300

    Clase Parametros

--- funciones.js
+++ funciones.js
@@ -1,11 +1,24 @@
 // definicion o declaracion de una funcion
 
-function saludar(){
-    console.log("Hola!!!")
+// function saludar(){
+//     console.log("Hola!!!")
+// }
+
+// // llamado a la funcion
+// saludar()
+// saludar()
+// saludar()
+// saludar()
+
+//------------PARAMETROS-------------
+
+// declaramos la funcion
+function suma(num1, num2){
+    let resultado = num1 + num2
+    console.log("El resultado es: " + resultado)
 }
 
-// llamado a la funcion
-saludar()
-saludar()
-saludar()
-saludar()
+// llamar la funcion
+suma(10, 5)
+suma(20, 3)
+suma(-100, 8)
