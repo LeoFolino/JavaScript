f6e7a001040b3752c2e135fbeb0b16e563176e09
LeoFolino <leonelfolinoing@gmail.com>
2024-09-05 12:39:45 -0300

    Clase condicionales multiples y anidados

--- condicionales.js
+++ condicionales.js
@@ -1,8 +1,49 @@
-let edad = 25
+// let edad = 25
 
-if(edad >= 18){
-    console.log("La persona es mayor de edad")
+// if(edad >= 18){
+//     console.log("La persona es mayor de edad")
+// }
+// else{
+//     console.log("La persona es menor de edad")
+// }
+
+// -----------CONDICIONALES ANIDADOS----------
+let clima = "soleado"
+let temp = 20
+let mensaje = ""
+
+if ( clima === "soleado" && temp >= 25){
+    console.log("El dia esta caluroso")
+} else {
+    console.log("El dia no esta caluroso")
+}
+
+let puntuacion = 80
+
+if(puntuacion >= 90){
+    console.log("Nota A")
+} 
+else if(puntuacion >= 80){
+    console.log("Nota B")
+}
+else if(puntuacion >= 70){
+    console.log("Nota C")
 }
 else{
-    console.log("La persona es menor de edad")
+    console.log("Nota D")
+}
+
+
+let = nota = -78
+
+if (nota >= 0 && nota <=10){
+    if(nota >= 7 ){
+        console.log("Aprobaste la materia")
+    }
+    else{
+        console.log("Tenes que recursar pa")
+    }
+}
+else{
+    console.log("La nota enviada es erronea! Debe ser del 1 al 10")
 }
