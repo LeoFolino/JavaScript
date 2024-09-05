72453b6d1f5b3b89664c8aec17225a2de122e5ba
LeoFolino <leonelfolinoing@gmail.com>
2024-09-05 13:03:03 -0300

    Clase Switch

--- condicionales.js
+++ condicionales.js
@@ -51,52 +51,104 @@
 //-----------IF TERNARIO--------------
 
 //tradicional vs ternario
-let numero = -5
+// let numero = -5
 
-if (numero > 0){
-    console.log("Es positivo") //if tradicional
+// if (numero > 0){
+//     console.log("Es positivo") //if tradicional
+// }
+// else{
+//     console.log("Es negativo")
+// }
+
+// (numero > 0) ? console.log("Es positivo"):console.log("Es negativo")  // if ternario
+
+// // asignacion a variable
+// let edad = 30
+// // let msj = ""
+
+// if(edad >= 18){
+//     msj = "Es mayor"
+// }
+// else{
+//     msj = "Es menor"
+// }
+
+// msj = (edad >= 18) ? "Es mayor":"Es menor" // Asignacion a variable
+// console.log(msj)
+
+
+// // mas de una instruccion
+// let Edad = 20
+// let mensaje = ""
+// let categoria = ""
+
+// if(edad >= 18){  // if tradicional
+//     mensaje = "Es mayor de edad"
+//     categoria = "Adulto"
+//     console.log("Felicidades, eres mayor de edad")
+// }
+// else{
+//     mensaje = "Es menor de edad"
+//     categoria = "Menor"
+//     console.log("Felicidades, eres menor de edad")
+// }
+
+// (edad >= 18) ? (
+//     mensaje = "Es mayor de edad",
+//     categoria = "Adulto",
+//     console.log("Felicidades, eres mayor de edad")) : (
+//         mensaje = "Es menor de edad",
+//         categoria = "Menor",
+//         console.log("Felicidades, eres menor de edad"))
+
+//---------SWITCH----------
+let diaSemana = 15
+let msj = ""
+
+switch(diaSemana){
+    case 1:
+        msj = "Hoy es Lunes"
+        break
+    case 2:
+        msj = "Hoy es Martes"
+        break
+    case 3:
+        msj = "Hoy es Miercoles"
+        break
+    case 4:
+        msj = "Hoy es Jueves"
+        break
+    case 5:
+        msj = "Hoy es Viernes"
+        break
+    case 6:
+        msj = "Hoy es Sabado"
+        break
+    case 7:
+        msj = "Hoy es Domingo"
+        break
+    default:
+        msj= "Ingreso un numero incorrecto"
 }
-else{
-    console.log("Es negativo")
-}
-
-(numero > 0) ? console.log("Es positivo"):console.log("Es negativo")  // if ternario
-
-// asignacion a variable
-let edad = 30
-// let msj = ""
-
-if(edad >= 18){
-    msj = "Es mayor"
-}
-else{
-    msj = "Es menor"
-}
-
-msj = (edad >= 18) ? "Es mayor":"Es menor" // Asignacion a variable
 console.log(msj)
 
+//--OTRO EJEMPLO--
 
-// mas de una instruccion
-let Edad = 20
-let mensaje = ""
-let categoria = ""
+let fruta = "asd"
+let msjFruta = ""
 
-if(edad >= 18){  // if tradicional
-    mensaje = "Es mayor de edad"
-    categoria = "Adulto"
-    console.log("Felicidades, eres mayor de edad")
+switch (fruta) {
+    case "manzana":
+        msjFruta = "Es una manzana"
+        break
+    case "banana":
+        msjFruta = "Es una banana"
+        break
+    case "naranja":
+        msjFruta = "Es una naranja"
+        break
+    default:
+        msjFruta = "Ingreso una fruta incorrecta"
+        break;
 }
-else{
-    mensaje = "Es menor de edad"
-    categoria = "Menor"
-    console.log("Felicidades, eres menor de edad")
-}
-
-(edad >= 18) ? (
-    mensaje = "Es mayor de edad",
-    categoria = "Adulto",
-    console.log("Felicidades, eres mayor de edad")) : (
-        mensaje = "Es menor de edad",
-        categoria = "Menor",
-        console.log("Felicidades, eres menor de edad"))
+console.log(msjFruta)
